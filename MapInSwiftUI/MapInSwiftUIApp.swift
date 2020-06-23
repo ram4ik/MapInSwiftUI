ce604981f634c3ebe8e14d3f77ac599c7ae8287e
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-06-23 07:46:47 +0300

    Initial Commit

---
+++ MapInSwiftUI/MapInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapInSwiftUIApp.swift
+//  MapInSwiftUI
+//
+//  Created by Ramill Ibragimov on 23.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct MapInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
